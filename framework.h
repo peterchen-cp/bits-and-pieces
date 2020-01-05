8653baf5ca8b3f45f0a724582b540f7f69897a14
peter <peter@klippel.de>
2020-01-05 20:50:04 +0100

    add empty project

---
+++ framework.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Selten verwendete Komponenten aus Windows-Headern ausschließen
+#define NOMINMAX
---
+++ pch.h
@@ -0,0 +1,7 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+#include <windows.h>
+
+#endif //PCH_H
